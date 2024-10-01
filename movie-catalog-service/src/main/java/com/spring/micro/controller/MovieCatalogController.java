79511ac0903eff2c5abcff3d0d942af57675efda
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-01 21:08:29 +0530

    rest call using web client in commented mode

--- movie-catalog-service/src/main/java/com/spring/micro/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/spring/micro/controller/MovieCatalogController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 import com.spring.micro.model.CatalogItem;
 import com.spring.micro.model.Movie;
@@ -20,13 +21,21 @@ public class MovieCatalogController {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
+	/*@Autowired
+	private WebClient.Builder webClientBuilder;*/
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
 		List<Rating> ratingsList = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
 		return ratingsList.stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+			// Alternate way to call rest using webclient
+			/*Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/" + rating.getMovieId())
+					.retrieve().bodyToMono(Movie.class).block();*/
+
 			return new CatalogItem(movie.getName(), "Description", rating.getRating());
 		}).collect(Collectors.toList());
 	}
