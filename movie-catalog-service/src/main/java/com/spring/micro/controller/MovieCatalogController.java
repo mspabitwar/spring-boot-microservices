6f97df9643440da5690e92658bb17b3066ec648b
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-01 16:16:28 +0530

    hardcoded rating details and resttemplate to call movie info API

--- movie-catalog-service/src/main/java/com/spring/micro/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/spring/micro/controller/MovieCatalogController.java
@@ -1,13 +1,17 @@
 package com.spring.micro.controller;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.spring.micro.model.CatalogItem;
+import com.spring.micro.model.Movie;
+import com.spring.micro.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,6 +19,13 @@ public class MovieCatalogController {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<Rating> ratingsList = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
+		return ratingsList.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Description", rating.getRating());
+		}).collect(Collectors.toList());
 	}
 }
