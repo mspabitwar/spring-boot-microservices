6939076179221a6203417ee58c9590288b4d4515
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-02 08:55:29 +0530

    Use service discovery to call APIs

--- movie-catalog-service/src/main/java/com/spring/micro/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/spring/micro/controller/MovieCatalogController.java
@@ -27,10 +27,10 @@ public class MovieCatalogController {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+		UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 		
 		return userRating.getRatings().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
 			// Alternate way to call rest using webclient
 			/*Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/" + rating.getMovieId())
