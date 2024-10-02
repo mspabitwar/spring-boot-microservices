49a1df9c3fe5a59849047807000033c67991d2a1
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-02 14:57:56 +0530

    rest call using timeout

--- movie-catalog-service/src/main/java/com/spring/micro/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/spring/micro/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 //import org.springframework.web.reactive.function.client.WebClient;
 
@@ -17,7 +18,11 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		/*HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setConnectTimeout(3000);
+		RestTemplate restTemplate = new RestTemplate(factory);*/
+		RestTemplate restTemplate =  new RestTemplate();
+		return restTemplate;
 	}
 	
 	/*@Bean
