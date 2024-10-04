9d4cf06afc0f74b17af6fe90c437f28aaf38e434
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-05 05:26:09 +0530

    default value + managing list & map

--- spring-boot-config/src/main/java/com/spring/config/GreetingController.java
+++ spring-boot-config/src/main/java/com/spring/config/GreetingController.java
@@ -1,5 +1,8 @@
 package com.spring.config;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,17 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting: default hello}")
 	private String greetingMessage;
 
 	@Value("${app.description}")
 	private String appDescription;
 
+	@Value("${my.list.value}")
+	private List<String> listValues;
+
+	@Value("#{${my.map.value}}")
+	private Map<String, String> mapValues;
+
 	@GetMapping("greeting")
 	public String greeting() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(greetingMessage).append("@").append(appDescription);
-		System.out.print(sb.toString());
+		sb.append(greetingMessage).append("@").append(appDescription).append("@").append(listValues).append("@")
+				.append(mapValues);
 		return sb.toString();
 	}
 }
